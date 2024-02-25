90bd4e681c3533c4b5c382c749d86e0287600d2a
Carla <carlusofi@gmail.com>
2024-02-25 18:40:45 -0500

    This is my number guessing game

---
+++ Number-guessing-game.py
@@ -0,0 +1,17 @@
+import random 
+
+def number_gen():
+  num = random.randint(1,10)
+  guess = int(input('Enter a number: '))
+  while guess!= num:
+    print("The number is " + str(num) + ". Try Again.\n")
+    guess = int(input('Enter a number: '))
+    num = random.randint(1, 100)  # Generate a new random number for the next iteration
+  else:
+    print("You guessed right!")
+    
+
+
+if __name__ == '__main__':
+  number_gen()
+  
